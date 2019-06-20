b2d46d088a5482dd6860b4a4d624fb692eaa30bb
“surajitsamui” <surajitsamui011@gmail.com>
2019-06-20 12:11:51 +0530

    angular practice

--- src/main/java/com/javainuse/controllers/TestController.java
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -5,6 +5,7 @@ import com.javainuse.service.EmployeeService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+@CrossOrigin("https://angular-jvmc7t.stackblitz.io")
 @RestController
 public class TestController {
 
