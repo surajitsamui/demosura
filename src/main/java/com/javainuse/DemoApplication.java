c0313b30e9a51772ab3c67ac0070576371138749
“surajitsamui” <surajitsamui011@gmail.com>
2019-07-10 18:33:39 +0530

    Asynchronus logger

--- src/main/java/com/javainuse/DemoApplication.java
+++ src/main/java/com/javainuse/DemoApplication.java
@@ -1,15 +1,61 @@
 package com.javainuse;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jni.Local;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass=true)
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@Slf4j
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+ 
+//    private static final Logger logger = LogManager.getLogger(DemoApplication.class);
+    private static Logger trxLogger = LoggerFactory.getLogger("com.javainuse");
+
+    public static void main(String[] args) {
+        String filePath = "/Users/surajit.samui/Desktop/payload_1.json";
+        System.out.println(readAllBytesJava7( filePath ) );
+        
+        String payloadText=readAllBytesJava7( filePath );
+//        System.out.println(payloadText);
+        log.info("Hi Surajit Samui world");
+        SpringApplication.run(DemoApplication.class, args);
+        log.info("Hi hello world");
+        log.debug("Hello, I'm DEBUG message.");
+        log.info("Hello, I'm INFO message.");
+        log.warn("Hello, I'm WARN message.");
+        log.error("Hello, I'm ERROR message.");
+        trxLogger.info("completed");
+        System.out.println(new Date());
+        System.out.println(LocalDateTime.now(ZoneId.systemDefault()));
+        
+    }
+    
+    private static String readAllBytesJava7(String filePath)
+    {
+        String content = "";
+ 
+        try
+        {
+            content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+ 
+        return content;
+    }
 
 }
--- src/main/java/com/javainuse/controllers/FileUploadController.java
+++ src/main/java/com/javainuse/controllers/FileUploadController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.javainuse.controllers;
 
 /**
@@ -14,7 +9,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
